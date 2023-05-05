c534f98675e5cbd73f64318ef82818eff0f5823f
shenkns <shenkns@gmail.com>
2023-05-05 16:57:20 +0300

    Save Prices To DB

--- BinanceDataLoader.py
+++ BinanceDataLoader.py
@@ -1,16 +1,33 @@
 import websocket
+import MySQLdb
+
 import logging
+import json
+import os
 
 
 logging.basicConfig(level=logging.DEBUG, 
                     filename="CryptoDataLoader.log",
-                    format="%(asctime)s %(levelname)s %(message)s"
+                    filemode="w",
+                    format="%(asctime)s %(levelname)s %(message)s",
+                    force=True
 )
 
 
 def on_message(ws, message):
-    print(message)
-    logging.debug(message)
+    data = json.loads(message)
+
+    db = MySQLdb.connect(host="localhost", user="root", passwd="", db="prices")
+    cursor = db.cursor()
+
+    for crypto_symbol in data:
+        cursor.execute(f"INSERT INTO `binance` (`symbol`, `price`) VALUES ('{crypto_symbol['s']}', {crypto_symbol['c']}) ON DUPLICATE KEY UPDATE `price` = {crypto_symbol['c']};")
+
+        print(f"{crypto_symbol['s']} = {crypto_symbol['c']}")
+        logging.debug(f"{crypto_symbol['s']} = {crypto_symbol['c']}")
+    
+    db.commit()
+    db.close()
 
 
 def on_close(ws):
